dcdbdfda5a5afe822c8c4105e43e1eed5b04d466
d9pe <marvinborck@Laptop-von-Marvin.local>
2023-06-28 09:52:39 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Marvin Testseite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
